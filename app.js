de2871502b6cd29fc0040fefba19a7acb8f6061a
VictorIzquier2 <victorizquier2@WorkStation.localdomain>
2020-12-16 09:33:07 +0100

    Conected to netlyfy

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((req,res,next) => {
 
 app.use(cors({
   credentials: true,
-  origin:["http://localhost:3001"]
+  origin:["http://localhost:3001", "https://victor-izquierdo-site.netlify.app"]
 }));
 
 app.use((req, res, next) => {
